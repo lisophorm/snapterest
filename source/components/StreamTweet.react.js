7591f8458e056f07b82094a042a02c4a2ce57a79
Alfonso Florio <alfonso.florio@wipro.com>
2017-05-26 15:32:13 +0100

    first commit

---
+++ source/components/StreamTweet.react.js
@@ -0,0 +1,24 @@
+/**
+ * Created by AL383171 on 26/05/2017.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Header = require('./Header.react');
+var Tweet = require('./Tweet.react');
+
+var StreamTweet = React.createClass({
+render:function() {
+    console.log('[Snapterest] StreamTweet:Running render()');
+    return (
+        <section>
+            <Header text={this.state.headerText} />
+            <Tweet
+                tweet={this.props.tweet}
+                onImageClick={this.props.onAddTweetToCollection}
+                />
+        </section>
+    );
+}
+});
+
+module.exports=StreamTweet;
